08b7c909652d64cb8e8d7a2131a3f3419cbeb5d1
albernhagen <andybernhagen@gmail.com>
2023-10-23 20:40:19 -0500

    POC image kit Add shadcn button UI

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+
+import { IKContext, IKImage } from "imagekitio-react";
+import { Button } from "@/components/ui/button";
+import "regenerator-runtime/runtime";
+import { useState } from "react";
+
 export default function Home() {
-  return <main>UNDER CONSTRUCTION</main>;
+  const [url, setUrl] = useState<string>("/20230721-112343.jpg");
+
+  function clickButton() {
+    setUrl("/20230716-203259.jpg");
+  }
+
+  return (
+    <>
+      <MyImage url={url}></MyImage>
+      <Button variant="outline" onClick={clickButton}>
+        Button
+      </Button>
+    </>
+  );
+}
+
+export function MyImage({ url }: { url: string }) {
+  return (
+    <IKContext
+      urlEndpoint="https://ik.imagekit.io/albernhagen"
+      publicKey="public_A2Vbt/wqy3V60ULQolSW7exHuVU="
+    >
+      <IKImage path={url} />
+    </IKContext>
+  );
 }
